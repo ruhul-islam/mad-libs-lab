15a4242bfd20ede79a2553a0925a442c64fe5c8e
ruhul-islam <islamruhul48092@gmail.com>
2022-08-02 07:28:15 -0400

    done

---
+++ src/context/StoriesContextProvider.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, useState } from "react";
+import Story from "../models/Story";
+import StoriesContext from "./StoriesContext";
+
+interface Props {
+  children: ReactNode;
+}
+
+const StoriesContextProvider = ({ children }: Props) => {
+  const [stories, setStories] = useState<Story[]>([
+    {
+      id: 1,
+      author: "Ruhul",
+      word1: "Detroit",
+      word2: "Usama",
+      word3: "asinine",
+      word4: "student",
+      word5: "7",
+      word6: "families first",
+    },
+    {
+      id: 2,
+      author: "Sadi",
+      word1: "Sylhet",
+      word2: "Usama",
+      word3: "asinine",
+      word4: "student",
+      word5: "19",
+      word6: "families first",
+    },
+  ]);
+  const addStory = (story: Story): void => {
+    setStories((prev) => [...prev, story]);
+  };
+
+  return (
+    <StoriesContext.Provider value={{ stories, addStory }}>
+      {children}
+    </StoriesContext.Provider>
+  );
+};
+
+export default StoriesContextProvider;
